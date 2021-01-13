7cf89d48284904b2dfb9104b8139a6b92bafa532
moondancer92 <70056397+moondancer92@users.noreply.github.com>
2021-01-13 17:35:44 +0900

    changed_Obj_created
    changed to Obj

--- timer.js
+++ timer.js
@@ -4,112 +4,104 @@ const timerNumber = document.querySelector(".js-timer_number"),
 	timerReset = document.querySelector(".timer_reset"),
 	timerStatus = document.querySelector(".js-timer_stauts");
 
-let start = false
-let savedTime 
-let difference 
-let tInterval  
-let takeBreak
+
+const eventHandler = {
+	
+	startTimer:	function () {
+			if(!eventHandler.startTimer.start){	
+			startTime = new Date().getTime();
+			tInterval = setInterval(eventHandler.getShowTime, 1000);
+			start = true;
+			}
+			else {
+			}
+			
+	},
+	puauseTimer: function (){
+		if(start){
+			clearInterval(tInterval)
+			eventHandler.startTimer.savedTime = difference
+			start = false;
+			timerPause.innerText = 'restart'
+	// pause the clock
+		}
+		else if (!eventHandler.startTimer.savedTime) {
+	// prevent pause button				
+		}
+		else {
+			eventHandler.startTimer();
+			timerPause.innerText ='pause'
+		}
+	// restart the clock	
+	},
+
+	resetTimer:function (){
+		if(!tInterval){
+		
+		}
+		else{
+			clearInterval(tInterval)
+			savedTime = null
+			timerNumber.innerText ="00:00:00" 
+			start =false;
+			timerPause.innerText ='pause'
+	
+		}
+	},
+
+	getShowTime:function () {
+		updatedTime = new Date().getTime();
+		if(!eventHandler.startTimer.savedTime){
+		difference = updatedTime - startTime;
+		}
+		else{
+		difference = eventHandler.startTimer.savedTime + updatedTime - startTime;
+		}
+	
+		hours = Math.floor(
+			(difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+		);
+		minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+		seconds = Math.floor((difference % (1000 * 60)) / 1000);
+		hours = hours < 10 ? "0" + hours : hours;
+		minutes = minutes < 10 ? "0" + minutes : minutes;
+		seconds = seconds < 10 ? "0" + seconds : seconds;
+	
+		timerNumber.innerHTML = hours + ":" + minutes + ":" + seconds;
+		
+		eventHandler.pomodoroEvt()
+	},
+
+	pomodoroEvt:function (){
+		if(!this.takeBreak){
+			timerStatus.innerText = 'Working Time!'
+			if(seconds == 10){
+				eventHandler.resetTimer();
+				timerStatus.innerText = 'Take a Break'
+				this.takeBreak = true
+				eventHandler.startTimer();
+			}	
+			//when working
+		}
+		else{
+			if(seconds == 10){
+				eventHandler.resetTimer();
+				timerStatus.innerText = 'Working Time!'
+				eventHandler.startTimer();
+				this.takeBreak = false
+			}
+			//when taking a break
+		}
+		}
+		
+}
 
 function btnEvt() {
 	
-	timerStart.addEventListener("click", startTimer);
-	timerPause.addEventListener("click",puauseTimer);
-	timerReset.addEventListener("click",resetTimer);
+	timerStart.addEventListener("click", eventHandler.startTimer);
+	timerPause.addEventListener("click",eventHandler.puauseTimer);
+	timerReset.addEventListener("click",eventHandler.resetTimer);
 }
-
-function startTimer() {
-
-	if(!start){	
-		startTime = new Date().getTime();
-		tInterval = setInterval(getShowTime, 1000);
-		start = true;
-		savedTime 
-	}
-	else {
-	}
-
-}
-function puauseTimer(){
-	if(start){
-		clearInterval(tInterval)
-		savedTime = difference
-		start = false;
-		timerPause.innerText = 'restart'
-// pause the clock
-	}
-	else if (!savedTime) {
-// prevent pause button				
-	}
-	else {
-		startTimer();
-		timerPause.innerText ='pause'
-	}
-// restart the clock	
-}
-
-function resetTimer(){
-	if(!tInterval){
-	
-	}
-	else{
-		clearInterval(tInterval)
-		savedTime = null
-		timerNumber.innerText ="00:00:00" 
-		start =false;
-		timerPause.innerText ='pause'
-
-	}
-}
-
-function getShowTime() {
-	updatedTime = new Date().getTime();
-	if(!savedTime){
-	difference = updatedTime - startTime;
-	}
-	else{
-	difference = savedTime + updatedTime - startTime;
-	}
-
-	hours = Math.floor(
-		(difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-	);
- 	minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-	seconds = Math.floor((difference % (1000 * 60)) / 1000);
-	hours = hours < 10 ? "0" + hours : hours;
-	minutes = minutes < 10 ? "0" + minutes : minutes;
-	seconds = seconds < 10 ? "0" + seconds : seconds;
-
-	timerNumber.innerHTML = hours + ":" + minutes + ":" + seconds;
-	
-	pomodoroEvt();
-
-}
-
-function pomodoroEvt(){
-	if(!takeBreak){
-		timerStatus.innerText = 'Working Time!'
-		if(seconds == 10){
-			resetTimer();
-			timerStatus.innerText = 'Take a Break'
-			startTimer();
-			 takeBreak = true
-		}	
-		//when working
-	}
-	else{
-		
-		if(seconds == 10){
-			resetTimer();
-			timerStatus.innerText = 'Working Time!'
-			startTimer();
-			takeBreak = false
-		}
-		//when taking a break
-	}
-	}
-
-
-
 
 function init() {
 	btnEvt();
